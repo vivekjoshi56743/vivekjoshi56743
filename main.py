faec8b88ba0adb48cecf58c5c7d811ffae87accd
vivekjoshi56743 <81894841+vivekjoshi56743@users.noreply.github.com>
2021-05-17 10:18:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_info():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            info = listener.recognize_google(voice)
+            print(info)
+            return info.lower()
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+
+def send_email(receiver, subject, message):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('joshivivek56743@gmail.com', 'hellohello123@')
+    email = EmailMessage()
+    email['From'] = 'joshivivek56743@gmail.com'
+    email['To'] = receiver
+    email['Subject'] = subject
+    email.set_content(message)
+    server.send_message(email)
+
+
+email_list = {
+    'red': 'bhattharsh144@gmail.com',
+    'bts': 'sarojbhattpwd@gmail.com',
+    'pink': 'jennie@blackpink.com',
+    'lisa': 'lisa@blackpink.com',
+    'irene': 'irene@redvelvet.com',
+}
+
+
+def get_email_info():
+    talk('To Whom you want to send email')
+    name = get_info()
+    receiver = email_list[name]
+    print(receiver)
+    talk('What is the subject of your email?')
+    subject = get_info()
+    talk('Tell me the text in your email')
+    message = get_info()
+    send_email(receiver, subject, message)
+    talk('Hey lazy ass. Your email is sent')
+    talk('Do you want to send more email?')
+    send_more = get_info()
+    if 'yes' in send_more:
+        get_email_info()
+
+
+get_email_info()
